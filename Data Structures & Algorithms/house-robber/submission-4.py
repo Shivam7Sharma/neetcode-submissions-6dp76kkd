e85264d7fd669d8ed36245a02fc49f76704e9fc2
shivam <ssharma4@wpi.edu>
2026-06-25 16:55:16 +0530

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # two options 1. rob i+1th house 2. rob i and i+2
+
+        memo=[-1]*len(nums)
+
+        def dfs(n):
+            if n>=len(nums):
+                return 0
+            elif memo[n]!=-1:
+                return memo[n]
+
+            memo[n]=max(dfs(n+1), nums[n] + dfs(n+2))
+
+            return memo[n]
+
+
+        return dfs(0)
